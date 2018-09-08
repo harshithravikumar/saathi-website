95163976dabc5c48941dfd3018fa3706d1394a9d
harshithravikumar <harshithravikumar@gmail.com>
2018-09-08 18:17:45 +0530

    website completed

--- saathi-website-master/saathi/saathiweb/static/js/main1.js
+++ saathi-website-master/saathi/saathiweb/static/js/main1.js
@@ -1,1 +1,4 @@
 $(".modal").each(function(l){$(this).on("show.bs.modal",function(l){var o=$(this).attr("data-easein");$(".modal-dialog").velocity("transition."+o)})});
+// $("#referral").click(function () {
+//     window.open("https://goo.gl/1dF1ST","_blank")
+// })
